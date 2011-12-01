6cc4bad2cf99ad6646f06dd704e15148f469a388
snake <bpchaoxin@163.com>
2011-12-01 14:33:09 +0800

    Initial Commit

---
+++ Persistence/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Persistence
+//
+//  Created by snake on 11-12-1.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Persistence/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Persistence
+//
+//  Created by snake on 11-12-1.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
